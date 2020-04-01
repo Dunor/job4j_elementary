c5fa00faf130bea765db085583bc2acd4da46e5e
Dunor <dunor@mail.ru>
2020-04-01 17:28:45 +0700

    3. Уникальность элементов и сортировка.[#158327]

--- src/ru/job4j/streamapi/Profiles.java
+++ src/ru/job4j/streamapi/Profiles.java
@@ -5,7 +5,8 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
-        List<Address> res = profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        List<Address> res = profiles.stream().map(Profile::getAddress).sorted((o1, o2) -> o1.getCity().compareTo(o2.getCity())).
+                distinct().collect(Collectors.toList());
         return res;
     }
 }
